54276302b9348f1ff09fff62ed1dcbebffc65ee2
Aaron Sananes <aasananes@gmail.com>
2014-01-31 08:56:23 +0000

    updated time format

--- mjolnir/Gruntfile.js
+++ mjolnir/Gruntfile.js
@@ -1,7 +1,10 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
+    clean: {
+        pre: ['dist/', 'build/'],
+        post: ['<%= archive_name %>.zip']
+    },
     sass: {
       options: {
         includePaths: ['assets/bower_components/foundation/scss']
@@ -15,7 +18,36 @@ module.exports = function(grunt) {
         }
       }
     },
-
+    copy: {
+        main: {
+            files: [
+                {expand: true, src: ['assets/css/**'], dest: 'build/'},
+                {expand: true, src: ['assets/bower_components/**'], dest: 'build/'},
+                {expand: true, src: ['assets/fonts/**'], dest: 'build/'},
+                {expand: true, src: ['assets/images/**'], dest: 'build/'},
+                {expand: true, src: ['assets/js/**'], dest: 'build/'},
+                {expand: true, src: ['partials/**'], dest: 'build/'},
+                {expand: true, src: ['assets/scss/**'], dest: 'build/'},
+                {expand: true, src: ['*', '!.gitignore', '!.DS_Store', '!build/'], dest: 'build/'},
+            ]
+        },
+        archive: {
+            files: [
+                {expand: true, src: ['<%= archive_name %>.zip'], dest: 'dist/'}
+            ]
+        }
+    },
+    compress: {
+        main: {
+            options: {
+                archive: '<%= archive_name %>.zip'
+            },
+            expand: true,
+            cwd: 'build/',
+            src: ['**/*'],
+            dest: ''
+        }
+    },
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
@@ -27,9 +59,13 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('bundle', ['copy:main', 'compress', 'copy:archive', 'clean:post']);
 }

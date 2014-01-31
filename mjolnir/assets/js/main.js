4ffaf13347d4b05a4024fa8d7ff41833120a614d
Aaron Sananes <aasananes@gmail.com>
2014-01-31 10:53:39 +0000

    huge update to theme

--- mjolnir/assets/js/main.js
+++ mjolnir/assets/js/main.js
@@ -9,7 +9,7 @@ $(document).foundation();
 
         function featured_image() {
 
-          var article = $('article.post');
+          var article = $('.home-template article.post');
 
           article.each( function(index) {
             var  img = $(this).find('img:first').remove(),
@@ -23,6 +23,15 @@ $(document).foundation();
 
         featured_image();
 
+        function post_featured_image() {
+          var post = $('.post-template article'),
+              img = post.find('img:first').remove(),
+               get_image = img.attr('src');
+
+          $('.blog_cover').css('background-image', 'url(' + get_image + ')');
+        }
+
+        post_featured_image();
 
         // Background check on
         // Specific images

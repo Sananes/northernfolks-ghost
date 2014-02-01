c1e179c00e4b29bd4effacd57e4161289c176e4b
Aaron Sananes <aasananes@gmail.com>
2014-02-01 11:20:34 +0000

    Huge updates

--- mjolnir/Gruntfile.js
+++ mjolnir/Gruntfile.js
@@ -1,10 +1,11 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
+    archive_name: grunt.option('name') || 'mjolnir',
+    
     sass: {
       options: {
-        includePaths: ['assets/bower_components/foundation/scss']
+        includePaths: ['assets/vendor/foundation/scss']
       },
       dist: {
         options: {
@@ -15,7 +16,48 @@ module.exports = function(grunt) {
         }
       }
     },
-
+    copy: {
+      main: {
+          files: [
+              {expand: false, src: ['assets/css/**'], dest: 'build/'},
+              {expand: false, src: ['assets/vendor/**'], dest: 'build/'},
+              {expand: false, src: ['assets/fonts/**'], dest: 'build/'},
+              {expand: false, src: ['assets/images/**'], dest: 'build/'},
+              {expand: false, src: ['!assets/js/**'], dest: 'build/'},
+              {expand: false, src: ['partials/**'], dest: 'build/'},
+              {expand: false, src: ['assets/scss/**'], dest: 'build/'},
+              {expand: false, src: ['*', '!.gitignore','!node_modules', '!.DS_Store'], dest: 'build/'},
+          ]
+      },
+      archive: {
+          files: [
+              {expand: true, src: ['<%= archive_name %>.zip'], dest: 'dist/'}
+          ]
+      }
+    },
+    compress: {
+        main: {
+            options: {
+                archive: '<%= archive_name %>.zip'
+            },
+            expand: true,
+            cwd: 'build/',
+            src: ['**/*'],
+            dest: ''
+        }
+    },
+    clean: {
+        pre: ['dist/', 'build/']
+    },
+    uglify: {
+      min: {
+        files: [{
+            expand: false,
+            src: ['assets/js/*.js'],
+            dest: 'build/assets/js/main.js'
+        }]
+      }
+    },
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
@@ -29,7 +71,13 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('bundle', ['clean', 'copy:main', 'uglify', 'compress', 'copy:archive']);
 }
--- mjolnir/assets/js/main.js
+++ mjolnir/assets/js/main.js
@@ -3,43 +3,195 @@ $(document).foundation();
 (function ($) {
     "use strict";
 
+    $(window).load( function() {
+      $('#purple-background').hide();
+      $('#articles').addClass('visible');
+    });
+
     $(document).ready(function(){
+        
+        $('#articles').addClass('hidden');
+
+        var config = {
+          'infinite_scrolling': true,         // Infinite Scrolling (Enable or Disable)
+          'disqus_shortname': 'vikingtheme',  // Disqus name
+          'analytics_id' : "",                // Google Analytics Id
+          'analytics_url': "",                // Google Analytics URL
+          'backgroundCheck': true             // Background Check
+        }
 
         $(".at_block").fitVids();
 
+        $(window).scroll(function () {
+
+            /* -- Endless Scrolling -- */
+
+            if (config.infinite_scrolling == true) {
+
+                $("#pagination").css("display", "none" );
+
+                var next_page = $("#pagination a.older_posts");
+                if (next_page.length > 0) {
+                    next_page = next_page.attr("href");
+                    if ($(window).scrollTop() == ($(document).height() - $(window).height())) {
+                        $(".endless").show();
+
+                        $.ajax({
+                            url: next_page,
+                            success: function (html) {
+                                if (html) {
+                                    var content = $(html),
+                                        posts = content.find(".article-post"),
+                                        pagination = content.find("#pagination"),
+                                        endless = content.find(".endless");
+
+                               
+                                    $("#pagination").remove();
+
+                                    $(".endless").remove();
+                                    posts.each(function (index) {
+                                        $(posts[index]).appendTo("#mainbody").addClass('animate fadeIn');
+                                    });
+
+                                    //Callback when create new article
+                                    featured_image();
+                                    $("#mainbody").append(endless);
+                                    $("#mainbody").append(pagination);
+                                    $("#pagination").css("display", "none");
+                                    $(".endless").hide(300);
+                                }
+                            }
+                        });
+                    } else {
+                        if ($("#pagination").css("display") == "block") {
+                            $(".no-more").removeClass("animate wobble");
+                            $(".no-more").addClass("animate wobble");
+                        }
+
+                    }
+                } else {
+                    $("#pagination").css("display", "none");
+                    // $("#pagination").html("<span class=\"button no-more\" style=\"opacity:1\">No more posts</span>");
+                    $(".no-more").removeClass("animate wobble");
+                    $(".no-more").addClass("animate wobble");
+                }
+            } else {
+                $("#pagination").css("display", "block");
+            }
+        });
+
+
+        // Featured Homepage posts images
+
+        // function featured_image() {
+
+        //   var article = $('.home-template article.post');
+
+        //   article.each( function() {
+        //     var  img = $(this).find('img[alt="featureimage"]').remove(),
+        //          get_image = img.attr('src');
+        //     //alert(get_image);
+        //     $(this).addClass('cover').css('background-image', 'url(' + get_image + ')');
+        //   }); 
+          
+        // }
+        //  featured_image();
+
+
         function featured_image() {
 
-          var article = $('.home-template article.post');
+            var post_links = $(".article-post .post-link"),
+                featured_html = $(".article-post"),
+                // featured_link = featured_html.find('a.featured-image-link'),
+                post_list_links = [];
 
-          article.each( function(index) {
-            var  img = $(this).find('img:first').remove(),
-                 get_image = img.attr('src');
-            //alert(get_image);
-            $(this).addClass('cover').css('background-image', 'url(' + get_image + ')');
-          });
+            if (post_links.length > 0) {
+    //            if ( Modernizr.localstorage ) {
+    //                localStorage.clear();
+    //            }
+                
+                post_links.each(function (index) {
+                    //Process script                 
+                    post_list_links.push(post_links[index].href);
+                    /* -- Next and Prev Post -- */
+                });
+            }
 
-          return false;
+            if (post_list_links.length > 0) {
+                post_list_links.forEach(function (element, index, array) {
+                    $.get(post_list_links[index], function (data) {
+                        var html = $(data),
+                            article = html.find("article.post .post_content"),
+                            img = article.find('img[alt="featureimage"]').attr('src'),
+                            video = article.find("iframe:first"),
+                            blog_cover = html.find(".blog_cover"),
+                            featured = featured_html[index];
+                            
+                            // Check if there is an image; this works with lazyload
+                            if(img) {
+                              $(featured).attr('data-original', img).css('background-image', 'url(' + img + ')');
+                            } else {
+                              $(featured).addClass('no-feature');
+                            }
+                    });
+                });
+            }
         }
 
-        featured_image();
+
+        //Featured Post page image
 
         function post_featured_image() {
           var post = $('.post-template article'),
-              img = post.find('img:first').remove(),
+              img = post.find('img[alt="featureimage"]').remove(),
                get_image = img.attr('src');
 
-          $('.blog_cover').css('background-image', 'url(' + get_image + ')');
+          $('.blog_cover').attr('data-original', img).css('background-image', 'url(' + get_image + ')');
         }
 
-        post_featured_image();
+       featured_image();
+       post_featured_image();
 
+
+ 
+
+
+
+
+
+
+
+        $('.article-post, .post_content img').lazyload({
+            effect : "fadeIn"
+        });
+      
+        // 
+ 
+
+        if($('body').hasClass('post-template')) {
+
+            $(".post-template article.post, .author-details .inner").stick_in_parent();
+
+            var disqus_shortname = config.disqus_shortname;
+
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        }
+
+        $('.back-to-top').click(function () {
+          $("html, body").animate({
+              scrollTop: 0
+          }, 600);
+          return false;
+        });
         // Background check on
         // Specific images
 
-        //  BackgroundCheck.init({
-        //   targets: '.target',
-        //   images: 'article.post'
-        // }); 
+
     });
 
 }(jQuery));
+
